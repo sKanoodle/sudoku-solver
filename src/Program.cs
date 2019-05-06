59e56434d592bc2d51735b900c4b6248f54cdce7
sKanoodle <sKanoodle@users.noreply.github.com>
2019-05-06 16:58:09 +0200

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sudoku easy = new Sudoku(new[] {
+                0,5,0,  0,0,0,  0,0,0,
+                9,0,7,  0,0,0,  2,0,0,
+                2,0,4,  0,6,0,  3,1,9,
+
+                3,0,0,  8,5,0,  0,0,6,
+                8,4,9,  0,1,0,  5,3,2,
+                6,0,0,  0,2,4,  0,0,1,
+
+                5,1,8,  0,7,0,  9,0,4,
+                0,0,2,  0,0,0,  7,0,3,
+                0,0,0,  0,0,0,  0,5,0,
+            });
+
+            Sudoku hard = new Sudoku(new[] {
+                0,0,0,  0,0,6,  0,0,5,
+                9,7,0,  1,8,0,  0,0,6,
+                0,0,0,  0,0,0,  1,0,0,
+
+                2,0,8,  0,0,1,  0,9,0,
+                4,0,7,  0,0,0,  6,0,1,
+                0,6,0,  5,0,0,  4,0,8,
+
+                0,0,9,  0,0,0,  0,0,0,
+                8,0,0,  0,4,9,  0,6,3,
+                6,0,0,  3,0,0,  0,0,0,
+            });
+
+            Sudoku evil = new Sudoku(new[] {
+                0,0,0,  0,0,0,  0,0,0,
+                0,6,0,  0,0,7,  2,5,0,
+                0,0,4,  1,5,0,  3,0,0,
+
+                0,2,0,  0,9,5,  6,0,0,
+                0,4,0,  0,0,0,  0,9,0,
+                0,0,3,  2,4,0,  0,7,0,
+
+                0,0,7,  0,6,8,  1,0,0,
+                0,3,6,  5,0,0,  0,8,0,
+                0,0,0,  0,0,0,  0,0,0,
+            });
+
+            SolveWithMetrics(easy);
+            SolveWithMetrics(hard);
+            SolveWithMetrics(evil);
+
+            Console.ReadKey();
+        }
+
+        private static void SolveWithMetrics( Sudoku sudoku )
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var result = Solve(sudoku);
+            watch.Stop();
+            result.Draw();
+            Console.WriteLine(watch.Elapsed);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        public static Sudoku Solve( Sudoku sudoku )
+        {
+            while (true)
+            {
+                //sudoku.Draw();
+
+                if (sudoku.UnsolvedTiles.Count == 0)
+                {
+                    return sudoku; // solved
+                }
+
+                // try easiest method of just looking for tiles with single possible value
+                switch (Solve1(sudoku))
+                {
+                    case SolveTryState.None: throw new ArgumentException();
+                    case SolveTryState.FoundAtLeastOne: continue;
+                    case SolveTryState.NoNewFindings: break;
+                    case SolveTryState.ImpossibleSolution: return null;
+                    default: throw new NotImplementedException();
+                }
+
+                // now get into recursion with guessing tiles and seeing if the sudoku is then solvable
+                Sudoku result = null;
+                if ((result = Solve2(sudoku)) != null)
+                    return result;
+
+                return null; // not solvable
+            }
+        }
+
+        private static SolveTryState Solve1(Sudoku sudoku)
+        {
+            bool didFindOne = false;
+            var tilesToCheck = sudoku.UnsolvedTiles.ToArray();
+            foreach (int tile in tilesToCheck)
+            {
+                var possibleValues = sudoku.Tiles[tile].CalculatePossibleValues(sudoku).ToArray();
+                if (possibleValues.Length < 1)
+                    return SolveTryState.ImpossibleSolution;
+                if (possibleValues.Length < 2)
+                {
+                    sudoku.Tiles[tile] = sudoku.Tiles[tile].WithValue(possibleValues[0]);
+                    sudoku.UnsolvedTiles.Remove(tile);
+                    didFindOne = true;
+                }
+            }
+            return didFindOne ? SolveTryState.FoundAtLeastOne : SolveTryState.NoNewFindings;
+        }
+
+        private static Sudoku Solve2(Sudoku sudoku)
+        {
+            var tilesToCheck = sudoku.UnsolvedTiles.ToArray();
+            foreach (int tile in tilesToCheck)
+            {
+                var possibleValues = sudoku.Tiles[tile].CalculatePossibleValues(sudoku).ToArray();
+                foreach (int value in possibleValues)
+                {
+                    var sudoku2 = sudoku.Clone();
+
+                    sudoku2.Tiles[tile] = sudoku2.Tiles[tile].WithValue(value);
+                    sudoku2.UnsolvedTiles.Remove(tile);
+
+                    Sudoku result;
+                    if ((result = Solve(sudoku2)) != null)
+                        return result;
+                }
+            }
+            return null;
+        }
+
+        enum SolveTryState { None, NoNewFindings, ImpossibleSolution, FoundAtLeastOne }
+    }
+}
---
+++ src/Sudoku.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SudokuSolver
+{
+    class Sudoku
+    {
+        public SudokuTile[] Tiles = new SudokuTile[81];
+
+        public HashSet<int> UnsolvedTiles;
+
+        private Sudoku() { }
+
+        public Sudoku(int[] numbers)
+        {
+            if (numbers.Length != 81) throw new ArgumentException();
+
+            UnsolvedTiles = Enumerable.Range(0, 81).ToHashSet();
+            for ( int i = 0; i < 81; i++)
+            {
+                int number = numbers[i];
+                if (number == 0)
+                    Tiles[i] = new SudokuTile(GetRowForTile(i), GetColForTile(i), GetNinthForTile(i));
+                else
+                {
+                    Tiles[i] = new SudokuTile(GetRowForTile(i), GetColForTile(i), GetNinthForTile(i), number);
+                    UnsolvedTiles.Remove(i);
+                }
+            }
+
+        }
+
+        public IEnumerable<SudokuTile> GetRow(int row)
+        {
+            if (row < 0 || row > 8) throw new IndexOutOfRangeException();
+
+            return Tiles.Skip(row * 9).Take(9);
+        }
+
+        public IEnumerable<SudokuTile> GetCol(int col)
+        {
+            if (col < 0 || col > 8) throw new IndexOutOfRangeException();
+
+            for (int i = 0; i < 9; i++)
+                yield return Tiles[col + i * 9];
+        }
+
+        public IEnumerable<SudokuTile> GetNinth(int ninth)
+        {
+            if (ninth < 0 || ninth > 8) throw new IndexOutOfRangeException();
+
+            int startRow = ninth / 3 * 3;
+            int startCol = ninth % 3 * 3;
+
+            for (int row = startRow; row < startRow + 3; row++)
+                for (int col = startCol; col < startCol + 3; col++)
+                {
+                    yield return Tiles[row * 9 + col];
+                }
+        }
+
+        private int GetRowForTile(int tile)
+        {
+            if (tile < 0 || tile > 80) throw new IndexOutOfRangeException();
+
+            return tile / 9;
+        }
+
+        private int GetColForTile(int tile)
+        {
+            if (tile < 0 || tile > 80) throw new IndexOutOfRangeException();
+
+            return tile % 9;
+        }
+
+        private int GetNinthForTile(int tile)
+        {
+            int col = GetColForTile(tile) / 3;
+            int row = GetRowForTile(tile) / 3;
+
+            return row * 3 + col;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    int tileIndex = row * 9 + col;
+
+                    if (Tiles[tileIndex].HasValue)
+                    {
+                        if (Tiles[tileIndex].IsGiven)
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                        else
+                            Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write(Tiles[tileIndex].Value);
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                    }
+                    else Console.Write(" ");
+
+                    if (col == 2 | col == 5)
+                        Console.Write("|");
+                    if (col == 8)
+                        Console.WriteLine();
+                }
+
+                if (row == 2 || row == 5)
+                    Console.WriteLine("---+---+---");
+            }
+        }
+
+        public Sudoku Clone()
+        {
+            Sudoku result = new Sudoku();
+            result.UnsolvedTiles = UnsolvedTiles.ToHashSet();
+            Array.Copy(Tiles, result.Tiles, Tiles.Length);
+            return result;
+        }
+    }
+
+    class SudokuTile
+    {
+        public int Row { get; }
+        public int Col { get; }
+        public int Ninth { get; }
+
+        public bool IsGiven { get; } = false;
+        public int Value { get; private set; }
+
+        public bool HasValue => IsGiven || Value != 0;
+
+        public SudokuTile(int row, int col, int ninth, int value)
+            : this(row, col, ninth)
+        {
+            IsGiven = true;
+            Value = value;
+        }
+
+        public SudokuTile(int row, int col, int ninth)
+        {
+            Row = row;
+            Col = col;
+            Ninth = ninth;
+        }
+
+        public SudokuTile WithValue(int value)
+        {
+            if (IsGiven) throw new Exception("this tile is given and cannot be changed!");
+            SudokuTile result = new SudokuTile(Row, Col, Ninth);
+            result.Value = value;
+            return result;
+        }
+
+        private static readonly int[] AllValues = Enumerable.Range(1, 9).ToArray();
+
+        public IEnumerable<int> CalculatePossibleValues(Sudoku sudoku)
+        {
+            if (HasValue) throw new Exception("already has value");
+
+            var takenValues = sudoku.GetRow(Row)
+                .Concat(sudoku.GetCol(Col))
+                .Concat(sudoku.GetNinth(Ninth))
+                .Where(t => t.HasValue)
+                .Select(t => t.Value);
+
+            return AllValues.Except(takenValues);
+        }
+    }
+}
